b6c49cbce31dffc4082de90ca90f5d3de2fa9def
Daniel Wirdehäll <daniel@textalk.se>
2018-02-06 11:26:00 +0100

    Added copy support for linux.

--- setup.bash
+++ setup.bash
@@ -3,7 +3,7 @@ rm ./setup.bash
 rm ./README.md
 rm -rf ./.git
 docker run --rm -v $(pwd):/app composer/composer create-project --prefer-dist laravel/laravel $1
-cp -r ./$1/ ./
+cp -r ./$1/* ./
 rm -rf ./$1
 mv .env.example .env
 mv ../${PWD##*/} ../$1
