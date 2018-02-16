6ffb5a4e0d0c010856fc7553420a0713b62449ab
Daniel Wirdehäll <daniel@textalk.se>
2018-02-16 14:18:35 +0100

    Uppdated support for both mac and linux.

--- setup.bash
+++ setup.bash
@@ -3,7 +3,23 @@ rm ./setup.bash
 rm ./README.md
 rm -rf ./.git
 docker run --rm -v $(pwd):/app composer/composer create-project --prefer-dist laravel/laravel $1
-cp -r ./$1/* ./
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine == 'Mac' ]
+then
+  cp -r ./$1/ ./
+else
+  cp -r ./$1/* ./
+fi
+
 rm -rf ./$1
 mv .env.example .env
 mv ../${PWD##*/} ../$1
